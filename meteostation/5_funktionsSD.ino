229b9a18eb7e4f967656b5e82315ddb6f17336e9
emil817 <75312001+emil817@users.noreply.github.com>
2021-05-16 19:29:10 +0700

    0.8

--- meteostation/5_funktionsSD.ino
+++ meteostation/5_funktionsSD.ino
@@ -4,26 +4,27 @@ void SD_Write()
   RTC.read(tm);
   if((tm.Minute == 0 || tm.Minute == 15 || tm.Minute == 30 || tm.Minute == 45) and tm.Minute != LMin)
   {
-    String nameF = GetFileName();
-    if(!SD.exists(nameF.c_str()))
+    String nameF =  ("M_" + String(tmYearToCalendar(tm.Year)) + "_" + String(tm.Month) + ".csv");
+    if(!SD.exists(nameF))
     {
-       File MFile = SD.open(nameF.c_str(), FILE_WRITE);
+       File MFile = SD.open(nameF, FILE_WRITE);
        MFile.println("Month;Day;Time;Temperature inside;Pressure;Temperature outside;Humidity;Light;");
        MFile.close();
     }
     LMin = tm.Minute;
-    File MFile = SD.open(nameF.c_str(), FILE_WRITE);
+    File MFile = SD.open(nameF, FILE_WRITE);
     String record = GetTime1() + String(bme.readTemperature()) + ";" + String(int((bme.readPressure() / 100.0F) * 0.75)) + ";" + String(sensors.getTempCByIndex(0))  + ";" + String(bme.readHumidity()) + ";" + String(int(analogRead(temt6000_pin) * 0.9765625)) + ";";
+    record.replace('.',',');
     MFile.println(record);
     MFile.close();
   }
 }
-String GetFileName()
+/*String GetFileName()
 {
     RTC.read(tm);
     String NameR = ("M_" + String(tm.Month) + "," + String(tmYearToCalendar(tm.Year)) + ".csv");
     return NameR;
-}
+}*/
 String GetTime1()
 {
   RTC.read(tm);
